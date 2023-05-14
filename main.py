58264221290c4c25a2a62c3f47ee18bfa2350fb6
Rafael M. Villela <rafaelmoralesvillela@gmail.com>
2023-05-14 18:04:56 -0300

    tirado códigos de testes de main.py

--- main.py
+++ main.py
@@ -2,13 +2,6 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-lista = [['oi','ola'], ['sim','nao']]
-for item in lista:
-    try:
-        print(item.index('sim'))
-    except:
-        print('n')
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
